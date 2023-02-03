dd6a9ad6fcbdf0b0e4dfe14514d2df4c1088d1f6
s1idewhist1e <66702601+s1idewhist1e@users.noreply.github.com>
2023-02-03 14:25:31 -0800

    fix simulator

--- sim/sim.py
+++ sim/sim.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 import sys, getopt, time
 
@@ -26,6 +26,8 @@ def main(argv):
     inputFile = argv[0]
     instructions = parse(open(inputFile).read())
 
+    print(instructions)
+
     execute(instructions)
 
 def parse(file):
@@ -47,72 +49,77 @@ def parse(file):
                 else:
                     labels[line[:-1]] = opsIndex
             else:
-                line = line.strip()
-                tokens = line.split()
-                match tokens[0]:
-                    case "NOP":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.NOP, None))
-                    case "STA":
-                        if len(tokens) != 2:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.STA, int(tokens[1], 16)))
-                    case "STB":
-                        if len(tokens) != 2:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.STB, int(tokens[1], 16)))
-                    case "LDA":
-                        if len(tokens) != 2:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.LDA, int(tokens[1], 16)))
-                    case "LDB":
-                        if len(tokens) != 2:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.LDB, int(tokens[1], 16)))
-                    case "SHL":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.SHL, None))
-                    case "SHR":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.SHR, None))
-                    case "AND":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.AND, None))
-                    case "INV":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.INV, None))
-                    case "INC":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.INC, None))
-                    case "LIA":
-                        if len(tokens) != 2:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.LIA, int(tokens[1], 16)))
-                    case "LIB":
-                        if len(tokens) != 2:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.LIB, int(tokens[1], 16)))
-                    case "BNE":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.BNE, None))
-                    case "JMP":
-                        if len(tokens) != 2:
-                            errors.append("Invalid number of tokens at line" + i)                        
-                        ops.append((constants.JMP, labels[tokens[1]]))
-                    case "OUT":
-                        if len(tokens) != 1:
-                            errors.append("Invalid number of tokens at line" + i)
-                        ops.append((constants.OUT, None))
-                    case other:
-                        errors.append("Invalid operation at line " + i)
                 opsIndex += 1
+
+    print(labels)
+
+    for i, line in enumerate(lines):
+        if not line.isspace() and line and line[0].isspace():
+            line = line.strip()
+            tokens = line.split()
+            match tokens[0]:
+                case "NOP":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.NOP, 0))
+                case "STA":
+                    if len(tokens) != 2:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.STA, int(tokens[1], 16)))
+                case "STB":
+                    if len(tokens) != 2:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.STB, int(tokens[1], 16)))
+                case "LDA":
+                    if len(tokens) != 2:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.LDA, int(tokens[1], 16)))
+                case "LDB":
+                    if len(tokens) != 2:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.LDB, int(tokens[1], 16)))
+                case "SHL":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.SHL, 0))
+                case "SHR":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.SHR, 0))
+                case "AND":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.AND, 0))
+                case "INV":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.INV, 0))
+                case "INC":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.INC, 0))
+                case "LIA":
+                    if len(tokens) != 2:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.LIA, int(tokens[1], 16)))
+                case "LIB":
+                    if len(tokens) != 2:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.LIB, int(tokens[1], 16)))
+                case "BNE":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.BNE, 0))
+                case "JMP":
+                    if len(tokens) != 2:
+                        errors.append("Invalid number of tokens at line" + i)                        
+                    ops.append((constants.JMP, labels[tokens[1]]))
+                case "OUT":
+                    if len(tokens) != 1:
+                        errors.append("Invalid number of tokens at line" + i)
+                    ops.append((constants.OUT, 0))
+                case other:
+                    errors.append("Invalid operation at line " + i)
     return ops
     
 def execute(instructions):
@@ -125,13 +132,15 @@ def execute(instructions):
     
     while pc < len(instructions):
 
-        time.sleep(0.1)
+        time.sleep(0.001)
 
         instr  = instructions[pc]
 
         areg &= 0xFF
         breg &= 0xFF
         pc &= 0xFF
+
+#        print(f"\tOperation: 0x{int(instr[0]):01x} 0x{instr[1]:02x}\tA Register: 0b{areg:08b}\tB Register: 0b{breg:08b}\tProgram Counter: 0x{pc:02x}\tmem: {mem}\t", end="")
 
         match instr[0]:
             case constants.NOP:
@@ -146,9 +155,9 @@ def execute(instructions):
                 breg = mem[int(instr[1])]
 
             case constants.SHL:
-                areg = areg << (instr[1] & 0b111)
+                areg = areg << (breg & 0b111)
             case constants.SHR:
-                areg = areg >> (instr[1] & 0b111)
+                areg = areg >> (breg & 0b111)
             case constants.AND:
                 areg = areg & breg
             case constants.INV:
@@ -162,13 +171,14 @@ def execute(instructions):
                 breg = instr[1]
 
             case constants.BNE:
-                if (areg & 0xFF == breg & 0xFF):
+                if (areg & 0xFF != breg & 0xFF):
                     pc += 1
             case constants.JMP:
-                pc = instr[1] & 0xF
+                pc = instr[1] & 0xFF
                 pc -= 1
 
             case constants.OUT:
+                print("output: ", end="")
                 for i in range(8):
                     if areg & (0b1 << 7 - i):
                         print("*", end = "")
@@ -179,6 +189,7 @@ def execute(instructions):
             case other:
                 raise Exception("Unknown operation!")
 
+#        print()
         pc += 1
             
 
